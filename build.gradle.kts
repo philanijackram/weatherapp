9bb2c68bba3f483ccb985643c8fda4ddd5a498a6
Philani Jack Ram <philanijackr@gmail.com>
2025-06-22 23:15:55 +0200

    Show forecast based on users location :GMS and HMS, HMS still need some tweaks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.huawei.agconnect:agcp:1.5.2.300")
+    }
+
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://developer.huawei.com/repo/")
+        }
+    }
+    dependencies {
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
