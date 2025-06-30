c778610038c2eb73c35b00b78a85cb9aeebed16c
Philani Jack Ram <philanijackr@gmail.com>
2025-07-01 00:50:09 +0200

    cleanup

--- app/src/main/java/com/dvt/weatherapp/MainActivity.kt
+++ app/src/main/java/com/dvt/weatherapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.dvt.weatherapp
 
 import android.Manifest
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -74,10 +73,6 @@ class MainActivity : ComponentActivity() {
                                     viewModel.getWeatherDataFromApi(
                                         location.latitude,
                                         location.longitude
-                                    )
-                                    Log.d(
-                                        "MainActivity",
-                                        "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
                                     )
                                 }
                             }
